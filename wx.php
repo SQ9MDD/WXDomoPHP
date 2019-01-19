5803aa3f822b3aee963816c75122554605115ea4
Rysiek Labus <rysieklabus@gmail.com>
2019-01-19 18:45:46 +0100

    Update wx.php

--- wx.php
+++ wx.php
@@ -1,8 +1,5 @@
 #!/usr/bin/php
 <?php
-// Temp IDX
-// Baromether IDX
-
 // ramka pogodowa ez pozycji
 // _mm dd gg mm		       temp		       baro
 // _03 29 06 58 c025 s009 g008 t030 r000 p000 P000 h00 b10218
@@ -12,18 +9,18 @@
 //
 // jesli nie podaje parametru wstawiam kropki
 // temp w fahrenheit F=(C-32)/9*5 lub mniej dokładnie F=(C-30)/2
+//
+//
+// zmienne tutaj ustawiasz parametry dostepu do danych  oraz znak i
+ pozycje
+$callsign	= 'SQ9MDD-4';			// your WX callsign
+$lat		= '5215.01N';			// coordinates APRS format
+$lon		= '02055.58E';			// coordinates APRS format
+$ip		= '10.9.48.3';			// domoticz IP adress
+$temp_idx	= '37';				// Temp sensor IDX
+$baro_idx	= '241';			// Baromether  IDX
 
-
-// zmienne tutaj ustawiasz parametry dostepu do danych  oraz znak i pozycje
-$callsign	= 'SQ9MDD-4';
-$lat		= '5215.01N';
-$lon		= '02055.58E';
-$ip		= '10.9.48.3';
-$temp_idx	= '37';
-$baro_idx	= '241';
-
-// nie edytuj ponizej tej linii chyba ze wiesz co robisz
-// url 
+///////////////// DO NOTE EDIT BELLOW THIS LINE /////////////////////////////
 $url            = 'http://'.$ip.'/json.htm?type=devices&rid=';
 
 //data i godzina
@@ -50,12 +47,7 @@ if($baro < 10000){
  $bzero = '';
 }
 
-//print data
-//echo "<pre>";
-
-//dane pogodowe bez pozycji
-//echo "_".$time."c...s...g...t".$temp."b".$baro;
-
 //dane pogodowe z pozycja
 echo "!".$lat."/".$lon."_.../...g...t".$zero.$temp."b".$baro." Test WX DOMOTICZ\n";
+
 ?>
